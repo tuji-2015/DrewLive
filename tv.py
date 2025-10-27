223d9ea9979799af489c43cb42804f7b2a05d7cd
DrewLive <andrew.rocha06@gmail.com>
2025-10-26 18:29:35 -0600

    Update tv.py

--- tv.py
+++ tv.py
@@ -173,7 +173,7 @@ async def scrape_all_sports_sections():
 def clean_m3u_header(lines):
     lines = [line for line in lines if not line.strip().startswith("#EXTM3U")]
     timestamp = int(datetime.utcnow().timestamp())
-    lines.insert(0, f'#EXTM3U url-tvg="http://drewlive24.duckdns.org:8081/DrewLive.xml.gz" # Updated: {timestamp}')
+    lines.insert(0, f'#EXTM3U url-tvg="https://github.com/Drewski2423/DrewLive/raw/refs/heads/main/DrewLive.xml.gz" # Updated: {timestamp}')
     return lines
 
 def replace_tv_urls(lines, tv_urls):
